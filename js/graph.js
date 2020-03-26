078668f13f9355b9ee87fd3846fa986b2226efc0
j-james <jj@j-james.me>
2020-03-25 21:13:10 -0700

    Add WA vs NY

--- js/graph.js
+++ js/graph.js
@@ -1,30 +1,50 @@
 // Washington State capita: 7614893
 // New York: 19453561
+//
+// front-end web development is a joke
 
 function plotStates(data) {
-    var date = [], positive = [], death = [];
+    var date = [], pos = [], death = [], posWA = [], posNY = [];
 
     for (var i = 0; i < data.length; i++) {
         row = data[i];
         if (row['state'] == 'WA') {
             date.push(row['date'] % 10000);
-            positive.push(row['positive']);
+            pos.push(row['positive']);
             death.push(row['death']);
+            posWA.push(row['positive'] / 7614893);
+        } else if (row['state'] == 'NY') {
+            posNY.push(row['positive'] / 19453561);
         }
     }
 
-    var data = [
-        {name: 'Positive Cases: WA', x:date, y:positive}, 
-        {name: 'Total Deaths: WA', x:date, y:death}
-    ];
-    var layout = {
-        title: 'COVID is bad',
-        xaxis: {
-            title: 'Date (month-day)'
-        }
-    };
+    Plotly.newPlot(
+        'wa',
+        [
+            {name:'Positive Cases', x:date, y:pos},
+            {name:'Total Deaths', x:date, y:death}
+        ],
+        {
+            title:'Washington State and COVID-19',
+            xaxis:{title: 'Date (month-day)'},
+            yaxis:{title: 'Positive Cases'}
+        },
+        {displaylogo: false}
+    );
 
-    Plotly.newPlot('map', data, layout, {displaylogo: false}); 
+    Plotly.newPlot(
+        'wany',
+        [
+            {name:'WA', x:date, y:posWA},
+            {name:'NY', x:date, y:posNY}
+        ],
+        {
+            title:'WA vs NY: Positive Cases per Capita',
+            xaxis:{title:'Date (month-day)'},
+            yaxis:{title:'Positive Cases divided by Population'}
+        },
+        {displaylogo: false}
+    );
 }
 
 Plotly.d3.csv('https://covidtracking.com/api/states/daily.csv', (data) => plotStates(data));
