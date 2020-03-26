e927037c1a098a1523aa812bf12629b82d46c4df
j-james <jj@j-james.me>
2020-03-25 19:40:12 -0700

    Label datasets

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,6 @@
+// Washington State capita: 7614893
+// New York: 19453561
+
 function plotStates(data) {
     var date = [], positive = [], death = [];
 
@@ -9,11 +12,19 @@ function plotStates(data) {
             death.push(row['death']);
         }
     }
-    Plotly.newPlot(
-        document.getElementById('map'),
-        [{x:date, y:positive}, {x:date, y:death}],
-        {title: 'COVID is bad - Washington State'}
-    );
+
+    var data = [
+        {name: 'Positive Cases', x:date, y:positive}, 
+        {name: 'Total Deaths', x:date, y:death}
+    ];
+    var layout = {
+        title: 'COVID is bad',
+        xaxis: {
+            title: 'Date'
+        }
+    };
+
+    Plotly.newPlot('map', data, layout, {displaylogo: false}); 
 }
 
 Plotly.d3.csv('https://covidtracking.com/api/states/daily.csv', (data) => plotStates(data));
