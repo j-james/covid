6fa12518e9e53c05f7d7f07d7ef113dbbfdc96ef
j-james <jj@j-james.me>
2020-04-05 15:03:44 -0700

    Fix dates

--- js/graph.js
+++ js/graph.js
@@ -1,20 +1,23 @@
 // Washington State capita: 7614893
 // New York: 19453561
 //
-// front-end web development is a joke
+// javascript development is a joke
 
-function plotStates(data) {
+function plotState(data) {
     var date = [], pos = [], death = [], posWA = [], posNY = [];
 
     for (var i = 0; i < data.length; i++) {
         row = data[i];
+        day = (row['date'] % 100);
+        month = ((row['date'] - day) / 100) % 100;
+        year = (((row['date'] - day) / 100) - month) / 100;
         if (row['state'] == 'WA') {
-            date.push(row['date'] % 10000);
+            date.push(year + '-' + month + '-' + day);
             pos.push(row['positive']);
             death.push(row['death']);
-            posWA.push(row['positive'] / 7614893);
+            posWA.push(row['positive']/7614893 * 100);
         } else if (row['state'] == 'NY') {
-            posNY.push(row['positive'] / 19453561);
+            posNY.push(row['positive']/19453561 * 100);
         }
     }
 
@@ -41,10 +44,15 @@ function plotStates(data) {
         {
             title:'WA vs NY: Positive Cases per Capita',
             xaxis:{title:'Date (month-day)'},
-            yaxis:{title:'Positive Cases divided by Population'}
+            yaxis:{title:'Positive Cases as a % of Population'}
         },
         {displaylogo: false}
     );
 }
 
-Plotly.d3.csv('https://covidtracking.com/api/states/daily.csv', (data) => plotStates(data));
+// function plotAll(data) {
+//     var date = [], pos = [], neg = [], hos = [], dead = [];
+// }
+
+Plotly.d3.csv('https://covidtracking.com/api/states/daily.csv', (data) => plotState(data));
+// https://covidtracking.com/api/states/daily.csv
