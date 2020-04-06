865e2fd91dfcd09a13f0e523baf07ad9b98a6298
j-james <jj@j-james.me>
2020-04-06 01:26:06 -0700

    Make code not shit
    Seriously improves ease-of-use and readability

--- js/graph.js
+++ js/graph.js
@@ -1,58 +1,67 @@
+// https://covidtracking.com/api/states/daily.csv
 // Washington State capita: 7614893
-// New York: 19453561
+// New York State capita: 19453561
 //
 // javascript development is a joke
 
-function plotState(data) {
-    var date = [], pos = [], death = [], posWA = [], posNY = [];
-
-    for (var i = 0; i < data.length; i++) {
-        row = data[i];
-        day = (row['date'] % 100);
-        month = ((row['date'] - day) / 100) % 100;
-        year = (((row['date'] - day) / 100) - month) / 100;
-        if (row['state'] == 'WA') {
-            date.push(year + '-' + month + '-' + day);
-            pos.push(row['positive']);
-            death.push(row['death']);
-            posWA.push(row['positive']/7614893 * 100);
-        } else if (row['state'] == 'NY') {
-            posNY.push(row['positive']/19453561 * 100);
-        }
-    }
+Plotly.d3.csv('https://covidtracking.com/api/states/daily.csv', function(data) { // (data) => plotState(data));
+    wa = state(data, 'WA');
+    ny = state(data, 'NY');
 
     Plotly.newPlot(
         'wa',
         [
-            {name:'Positive Cases', x:date, y:pos},
-            {name:'Total Deaths', x:date, y:death}
+            {name:'Positive Cases', x:wa[0], y:wa[1]},
+            {name:'Total Deaths', x:wa[0], y:wa[3], line:{color: 'red'}}
         ],
         {
             title:'Washington State and COVID-19',
-            xaxis:{title: 'Date (month-day)'},
-            yaxis:{title: 'Positive Cases'}
+            legend:{x:0.02,y:1.05},
+            xaxis:{title:'Date (month-day)'},
+            yaxis:{title:'Positive Cases'}
         },
-        {displaylogo: false}
+        {displayModeBar: false}
     );
+
+    for (i = 0; i < wa[1].length; i++) wa[1][i] /= 76148.93;
+    for (i = 0; i < ny[1].length; i++) ny[1][i] /= 194535.61;
 
     Plotly.newPlot(
         'wany',
         [
-            {name:'WA', x:date, y:posWA},
-            {name:'NY', x:date, y:posNY}
+            {name:'WA', x: wa[0], y: wa[1], line: {color: 'green'}},
+            {name:'NY', x: wa[0], y: ny[1], line: {color: 'blue'}}
         ],
         {
-            title:'WA vs NY: Positive Cases per Capita',
+            title:'WA vs NY (vs CA): Positive Cases per Capita',
+            legend:{x:0.02,y:1.00},
             xaxis:{title:'Date (month-day)'},
             yaxis:{title:'Positive Cases as a % of Population'}
         },
-        {displaylogo: false}
+        {displayModeBar: false}
     );
+});
+
+function state(data, state) {
+    var date = [];
+    var pos = [], neg = [], dead = [];
+    var hos = [], icu = [], rec = [];
+
+    for (i = 0; i < data.length; i++) {
+        row = data[i];
+        day = (row['date'] % 100);
+        month = ((row['date'] - day) / 100) % 100;
+        year = (((row['date'] - day) / 100) - month) / 100;
+        if (row['state'] == state) {
+            date.push(year + '-' + month + '-' + day);
+            pos.push(row['positive']);
+            neg.push(row['negative']);
+            dead.push(row['death']);
+            hos.push(row['hospitalized']);
+            icu.push(row['inIcuCumulative']);
+            rec.push(row['recovered']);
+        }
+    }
+    total = [date, pos, neg, dead, hos, icu, rec];
+    return total;
 }
-
-// function plotAll(data) {
-//     var date = [], pos = [], neg = [], hos = [], dead = [];
-// }
-
-Plotly.d3.csv('https://covidtracking.com/api/states/daily.csv', (data) => plotState(data));
-// https://covidtracking.com/api/states/daily.csv
