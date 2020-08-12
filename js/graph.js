9e4f088503a7cc7b966afa335df5beb01ceb4fe5
j-james <35242550+j-james@users.noreply.github.com>
2020-08-11 22:28:18 -0700

    Make dataset issues less visable

--- js/graph.js
+++ js/graph.js
@@ -207,7 +207,10 @@ function state(data, state) {
 		}
 	}
 	for (i = 0; i < pos.length-1; i++) {
-		ddx.push(pos[i] - pos[i+1]); // This dataset starts with the most recent values
+		if (pos[i] - pos[i+1]) > 0
+			ddx.push(pos[i] - pos[i+1]); // This dataset starts with the most recent values
+		else
+			ddx.push(ddx[ddx.length - 1]);
 	}
 	ddx.push(0);
 	for (i = 0; i < ddx.length-1; i++) {
